2423f447f0274f7f524ef6d939a0b36c675b15e3
Sasha246sasha <sasha.dolgopolov2012@gmail.com>
2024-10-22 21:50:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup, find_packages
 
-
-def readme():
-    with open('README.md', 'r') as f:
-        return f.read()
-
-
 setup(
   name='l-converters',
   version='1.0.1',
